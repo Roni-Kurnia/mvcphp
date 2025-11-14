5bcdca50e463f1ffaef83cb2ce2e5047fe7df3e1
Roni Kurnia <roni.kurnia4249@gmail.com>
2025-11-14 10:29:23 +0700

    Routing: mengatur url dengan menggunakan htaccess dan membersihkannya menggunakan beberapa function

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,16 @@
 
 class App {
     public function __construct() {
-        echo "OK!";
+        $url = $this->getUrl();
+        var_dump($url);
     }
-};
 
-?>  
+    public function getUrl() {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
